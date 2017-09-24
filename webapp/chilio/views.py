99a731579c6ac1af7c0030e2f94bdf83462edfc7
Jasper Boomer <jasperboomer@gmail.com>
2017-09-24 19:44:43 +0200

    Display time points from db in chartjs

--- webapp/chilio/views.py
+++ webapp/chilio/views.py
@@ -1,18 +1,46 @@
 import datetime
+import sqlite3
+import time
 from chilio.app import app
 from flask import render_template
+from flask import g as flask_global
+
+DATABASE = 'test.db'
+
+def get_db():
+    """ DB Connection singleton """
+    db = getattr(flask_global, '_database', None)
+    if db is None:
+        db = flask_global._database = sqlite3.connect(DATABASE)
+        db.row_factory = sqlite3.Row
+    return db
+
+def query_db(query, args=(), one=False):
+    """ Execute single query 
+        
+        :param query: SQL statement string
+        :param args:  Variables
+        :param one:   Set to True to receive only one row
+    """
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(flask_global, '_database', None)
+    if db is not None:
+        db.close()
 
 @app.route('/')
 @app.route('/index')
 def index():
-    # Mock values
-    samples = [
-        { 'time' : '13:06:34'
-          , 'value' : 1023}
-      , { 'time' : '13:07:27'
-          , 'value' : 1019}
-      , { 'time' : '13:09:19'
-          , 'value' : 1012}
-    ]
+    results = query_db('SELECT * FROM sensorvals;')
+    samples = [{'tmilliseconds' : row['time'] * 1000
+              , 'value' : row['value']
+              , 'tstring' : time.strftime('%Y-%m-%d %H:%M:%S'
+                                          , time.localtime(row['time']))}
+                    for row in results]
 
     return render_template('index.html', samples=samples)
