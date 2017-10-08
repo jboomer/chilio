fdf1bcef069d8b991e0ba9e6d7d9ae7ea989e5b9
Jasper Boomer <jasperboomer@gmail.com>
2017-10-08 19:23:02 +0200

    Use object instead of template for data

--- webapp/chilio/static/js/plot.js
+++ webapp/chilio/static/js/plot.js
@@ -1,28 +1,26 @@
-var ctx = document.getElementById("plotSensor").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: "scatter",
-    data: {
-        datasets: [{
-            label: "Moisture",
-            data: [
-                {% for sample in samples %}
-                {x: new Date( {{ sample.tmilliseconds }} ), y: {{ sample.value }} },
-                {% endfor %}
-            ],
-            fill: false,
-            showLine: true,
-        }]
-    },
-    options: {
-        scales: {
-            xAxes: [{
-                type: "time",
-                display: true,
-                time: {
-                    unit: 'second'
-                }
+function plotdata(canvas, mydata) {
+    var ctx = canvas.getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: "scatter",
+        data: {
+            datasets: [{
+                label: "Moisture",
+                data: mydata,
+                fill: false,
+                showLine: true,
             }]
-
+        },
+        options: {
+            scales: {
+                xAxes: [{
+                    type: "time",
+                    display: true,
+                    time: {
+                        unit: 'second'
+                    }
+                }]
+    
+            }
         }
-    }
-});
+    });
+}
