3f803d821d2e46d69a9238c2114981874a124692
Jasper Boomer <jasperboomer@gmail.com>
2018-04-29 15:58:19 -0400

    Clean up JS

--- webapp/chilio/static/js/main.js
+++ webapp/chilio/static/js/main.js
@@ -1,50 +1,62 @@
-var ctx = document.getElementById("plotSensor").getContext('2d');
-var $upbtn = $('#btnUpdate');
 
-$upbtn.click(function() {
-    $.getJSON('_api/sensorvals/?sensorid=0', function (samples) {
-        update_data(samples)
-    });
+function get_update_data_function(chartObject, sensorid) {
 
-});
-
-var myChart = new Chart(ctx, {
-    type: "scatter",
-    data: {
-        datasets: [{
-            label: "Moisture",
-            data: data,
-            borderColor: "rgba(0,100,220,1)",           // Color of line between dots
-            pointBackgroundColor: "rgba(0,100,220,1)",  // Color of dots
-            fill: false,
-            showLine: true,
-            lineTension: 0.3,
-        }]
-    },
-    options: {
-        scales: {
-            xAxes: [{
-                type: "time",
-                display: true,
-                time: {
-                    unit: 'second'
-                }
-            }]
-
-        }
+    var ret = function update_data() {
+        $.getJSON('_api/sensorvals/?sensorid=' + sensorid, fill_data);
     }
-});
 
-function update_data(samples) {
-    var data = [];
-    samples.forEach(
-        function (elem, index) {
-            var datapoint = 
-                {x: new Date(elem.TIME * 1000.0), y: elem.VALUE};
-            data.push(datapoint);
+    function fill_data(samples) {
+        var data = [];
+        samples.forEach(
+            function (elem, index) {
+                var datapoint = 
+                    {x: new Date(elem.TIME * 1000.0), y: elem.VALUE};
+                data.push(datapoint);
+        });
+
+
+        chartObject.data.datasets[0].data = data;
+        chartObject.update();
+    }
+    
+    return ret;
+}
+
+
+$(function () {
+    var ctx = document.getElementById("plotSensor").getContext('2d');
+    
+    var myChart = new Chart(ctx, {
+        type: "scatter",
+        data: {
+            datasets: [{
+                label: "Moisture",
+                data: [],
+                borderColor: "rgba(0,100,220,1)",  // Color of line between dots
+                pointBackgroundColor: "rgba(0,100,220,1)",  // Color of dots
+                fill: false,
+                showLine: true,
+                lineTension: 0.3,
+            }]
+        },
+        options: {
+            scales: {
+                xAxes: [{
+                    type: "time",
+                    display: true,
+                    time: {
+                        unit: 'second'
+                    }
+                }]
+
+            }
+        }
     });
 
+    var updateFunc = get_update_data_function(myChart, 0);
 
-    myChart.data.datasets[0].data = data;
-    myChart.update();
-}
+    $('#btnUpdate').click(updateFunc);
+
+    $('#btnUpdate').click();
+
+});
